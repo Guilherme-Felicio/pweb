1757edd93a992716052cb08e5c0c4c161fc2ffde
Felicio <guifelicio7@gmail.com>
2021-09-08 21:39:40 -0300

    Adicionado a atividade 6

---
+++ Atividade6/script.js
@@ -0,0 +1,8 @@
+const nota1 = prompt("Ola, por favor digite a nota 1");
+const nota2 = prompt("Ola, por favor digite a nota 2");
+const nota3 =  prompt("Ola, por favor digite a nota 3");
+
+let total = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3;
+
+alert("Sua Média é: " + total)
+    
